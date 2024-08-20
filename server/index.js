b70ea202a3e8a2f74a88245112c80ae1abdd10f2
manoj <manoj.gogulamudi@dvara.com>
2024-08-20 11:37:34 +0530

    dotenv file added to hide the db access url

--- server/index.js
+++ server/index.js
@@ -8,9 +8,12 @@ import expressGraphqlUploads from 'graphql-upload/graphqlUploadExpress.mjs'
 import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs'
 import { type } from 'os'
 import { MongoClient } from 'mongodb'
+import {config} from 'dotenv'
+config()
 // import Upload from 'graphql-upload/Upload.mjs'
 
-const CON_STR = 'mongodb+srv://pankajsainipank669:2osCmZiUlrqOGP0A@cluster0.qr6yq.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0'
+// const CON_STR = 'mongodb+srv://pankajsainipank669:2osCmZiUlrqOGP0A@cluster0.qr6yq.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0'
+const CON_STR = process.env.URI
 const app = express()
 const PORT = 5000
 app.use(expressGraphqlUploads())
